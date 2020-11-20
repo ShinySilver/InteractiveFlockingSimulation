c8dc8d18a3f3fa9870b1bb4754ae8e2a66b31db3
ShinySilver <nicolas.louismarie@gmail.com>
2020-11-20 15:34:16 +0100

    Modified project structure

--- agents/agent.py
+++ agents/agent.py
@@ -1,5 +1,8 @@
+from abc import ABC
+import numpy as np
 
-class IAgent:
+
+class Agent(ABC):
     def __init__(self, pos=(0,0,0), focus:float=1.0, nimbus:float=1.0):
         """
 
@@ -8,17 +11,20 @@ class IAgent:
         focus @optional is the can-see distance modifier
         nimbus @optional is the can-be-seen distance modifier
         """
-        self._focus = focus
-        self._nimbus = nimbus
-        self._pos = pos
+        self.squared_focus = focus**2
+        self.squared_nimbus = nimbus**2
+        self.pos = np.array(pos)
 
     def can_focus(self, agent):
-        return abs(self._pos-agent.pos)
+        return np.sum(self.pos*self.pos-agent._pos*agent._pos)*self.squared_focus<1.0
 
-    
+    def in_nimbus_of(self, agent):
+        return np.sum(self.pos*self.pos-agent._pos*agent._pos)*agent.squared_nimbus<1.0
 
+    @abs
     def render(self):
         raise NotImplementedError()
 
+    @abs
     def update(self):
         raise NotImplementedError()
---
+++ agents/balise_agent.py
@@ -0,0 +1,9 @@
+from agents.agent import Agent
+
+
+class BaliseAgent(Agent):
+    def update(self):
+        pass
+
+    def render(self):
+        pass
