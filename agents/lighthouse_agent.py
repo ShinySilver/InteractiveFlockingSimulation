9c8704c310ba8b08293a69ee2c8a2d5b9f959e33
ShinySilver <nicolas.louismarie@gmail.com>
2021-01-05 14:36:07 +0100

    Improved lighthouse

--- agents/lighthouse_agent.py
+++ agents/lighthouse_agent.py
@@ -2,10 +2,11 @@ from agents.agent import Agent
 
 
 class LighthouseAgent(Agent):
-    def __init__(self, color, context, pos=(0, 0), focus: float = 1.0, nimbus: float = 1.0):
+    def __init__(self, color, context, pos=(0, 0), focus: float = 1.0, nimbus: float = 1.0, hwidth: int = 10):
         super().__init__(context, pos, focus, nimbus)
         self.color = color
         self.id = None
+        self.hwidth = hwidth
 
     def prepare_update(self):
         pass
@@ -15,11 +16,11 @@ class LighthouseAgent(Agent):
 
     def render(self, client):
         if self.id is None:
-            self.id = (client.create_oval(self.pos[0] - 10, self.pos[1] - 10, self.pos[0] + 10, self.pos[1] + 10,
+            self.id = (client.create_oval(self.pos[0] - self.hwidth, self.pos[1] - self.hwidth, self.pos[0] + self.hwidth, self.pos[1] + self.hwidth,
                                           fill=self.color, outline="yellow"),
                        client.create_oval(self.pos[0] - self.nimbus, self.pos[1] - self.nimbus, self.pos[0] + self.nimbus,
                                           self.pos[1] + self.nimbus, outline="yellow"))
             return
-        client.coords(self.id[0], self.pos[0] - 10, self.pos[1] - 10, self.pos[0] + 10, self.pos[1] + 10)
+        client.coords(self.id[0], self.pos[0] - self.hwidth, self.pos[1] - self.hwidth, self.pos[0] + self.hwidth, self.pos[1] + self.hwidth)
         client.coords(self.id[1],self.pos[0] - self.nimbus, self.pos[1] - self.nimbus, self.pos[0] + self.nimbus,
                                           self.pos[1] + self.nimbus)
